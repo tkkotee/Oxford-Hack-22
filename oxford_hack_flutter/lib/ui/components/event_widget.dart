ff2498e22da152b23de459120161d9908ca26bfb
paolosait <paolosait7@gmail.com>
2022-11-12 13:09:35 +0000

    PS: Created shell event widget

---
+++ oxford_hack_flutter/lib/ui/components/event_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EventWidget extends StatelessWidget {
+  const EventWidget({super.key, required this.eventTitle, required this.startTime, required this.endTime});
+
+  final String eventTitle;
+  final String startTime;
+  final String endTime;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
